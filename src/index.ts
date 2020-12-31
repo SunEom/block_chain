84b9d0ff8c135c5c238009baf1a328585ee9bc2e
SunEom <dkxl0701@gmail.com>
2020-12-31 20:51:25 +0900

    Create Blockchain #2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { time } from 'console';
+import * as CryptoJS from 'crypto-js';
 
 class Block {
   public index: number;
@@ -6,6 +6,10 @@ class Block {
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
     this.index = index;
     this.hash = hash;
@@ -17,8 +21,11 @@ class Block {
 
 const genesisBlock: Block = new Block(0, '2020202020200', '', 'Hello', 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
 
+const getLastestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 export {};
