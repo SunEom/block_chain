0a2632097f98ac173dcba04bdc2f855bc6c82c43
SunEom <dkxl0701@gmail.com>
2020-12-31 21:54:04 +0900

    Finish!

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,7 @@ const createNewBlock = (data: string): Block => {
   const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
   const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
   addBlock(newBlock);
+
   return newBlock;
 };
 
@@ -68,5 +69,11 @@ const addBlock = (candidateBlock: Block): void => {
     blockchain.push(candidateBlock);
   }
 };
+
+createNewBlock('second block');
+createNewBlock('third block');
+createNewBlock('fourth block');
+
+console.log(blockchain);
 
 export {};
